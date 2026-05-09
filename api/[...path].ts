34f6498472ea3f39c1b2de5e25ef3b87d9fd52bc
cyber01s <138607733+cyber01s@users.noreply.github.com>
2026-05-09 22:19:10 +0100

    delete

--- api/[...path].ts
+++
@@ -1,8 +0,0 @@
-import app from '../server';
-
-export default (req: any, res: any) => {
-  if (process.env.VERCEL) {
-    console.log(`[API Proxy] Request for: ${req.url}`);
-  }
-  return app(req, res);
-};
